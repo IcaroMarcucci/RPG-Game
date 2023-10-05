253b800f535552f63864b170e940b8034d1f500b
Icaro Marcucci <icaro.marcucci@gmail.com>
2023-10-05 20:22:59 -0300

    Day 3
    Improving the map

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RPG-Game
  
+A 2D-style RPG in Python that includes a lot of elements you need for a sophisticated game like graphics and animations, fake depth; upgrade mechanics, a level map and quite a bit more. 
+
+
+### Credits: The art assets and the soundtrack have been done by Pixel-boy and AAA and can be found here: https://pixel-boy.itch.io/ninja-adventure-asset-pack They are published under a CC0 license
--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import *
 from player import *
+from support import *
 
 class Level:
     def __init__(self):
@@ -14,14 +15,19 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x,y),[self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+           'boundary': import_csv_layout('map\map_FloorBlocks.csv')
+       }
+        for style,layout in layouts.items():
+           
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),self.obstacle_sprites,'invisible')
+        self.player = Player((2000,1430),[self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         #Update and draw
@@ -36,11 +42,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        #creating floor
+        self.floor_surf = pygame.image.load('../RPG-Game/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
     def custom_draw(self,player):
 
         #Get offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        #drawing floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
